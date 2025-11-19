0694df11767370de3837421d91e9672eab9a6e6b
J1nu-Lo3 <jcezar61@yahoo.com>
2025-11-19 17:42:12 +0100

    update du js

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -3,24 +3,29 @@ const filtersContainer = document.querySelector(".categories-buttons");
 
 let allWorks = [];
 
-// projets gallerie
-fetch("http://localhost:5678/api/works")
-  .then((res) => res.json())
-  .then((works) => {
-    allWorks = works;
-    displayWorks(works);
-  });
+// fonction fecth pour la gallerie
+function fetchWorks() {
+  return fetch("http://localhost:5678/api/works")
+    .then((res) => res.json())
+    .catch((err) => console.error("Erreur :", err));
+}
+
+// fonction button
+function fetchCategories() {
+  return fetch("http://localhost:5678/api/categories")
+    .then((res) => res.json())
+    .catch((error) => console.error("Erreur fetch categories :", error));
+}
 
 // fonction pour afficher les photos de la gallerie
 function displayWorks(works) {
-  gallery.innerHTML = "";
-
-  works.forEach((work) => {
+  for (let work of works) {
     const figure = document.createElement("figure");
-
+    figure.dataset.category = work.categoryId;
     const img = document.createElement("img");
     img.src = work.imageUrl;
     img.alt = work.title;
+    img.loading = "lazy";
 
     const caption = document.createElement("figcaption");
     caption.textContent = work.title;
@@ -28,43 +33,49 @@ function displayWorks(works) {
     figure.appendChild(img);
     figure.appendChild(caption);
     gallery.appendChild(figure);
-  });
+  }
 }
 
-// categories button
-fetch("http://localhost:5678/api/categories")
-  .then((res) => res.json())
-  .then((categories) => {
-    createFilterButtons(categories);
+// filtre sans avoir a détruire le DOM
+function filterGallery(categoryId) {
+  const figures = gallery.querySelectorAll("figure");
+
+  figures.forEach((fig) => {
+    const figCat = Number(fig.dataset.category);
+
+    if (categoryId === "all" || figCat === categoryId) {
+      fig.style.display = ""; // visible
+    } else {
+      fig.style.display = "none"; // masqué
+    }
   });
+}
 
 // boultons categories dynamique
 function createFilterButtons(categories) {
   const btnAll = document.createElement("button");
+
   btnAll.textContent = "Tous";
   btnAll.classList.add("active");
+
   btnAll.addEventListener("click", () => {
     setActiveButton(btnAll);
-    displayWorks(allWorks);
+    filterGallery("all");
   });
 
   filtersContainer.appendChild(btnAll);
 
-  categories.forEach((category) => {
+  for (let category of categories) {
     const btn = document.createElement("button");
     btn.textContent = category.name;
-    btn.dataset.id = category.id;
 
     btn.addEventListener("click", () => {
       setActiveButton(btn);
-      const filtered = allWorks.filter(
-        (work) => work.categoryId === category.id
-      );
-      displayWorks(filtered);
+      filterGallery(category.id);
     });
 
     filtersContainer.appendChild(btn);
-  });
+  }
 }
 
 // button active
@@ -75,3 +86,13 @@ function setActiveButton(activeBtn) {
 
   activeBtn.classList.add("active");
 }
+
+// Lancement du script
+fetchWorks().then((works) => {
+  allWorks = works;
+  displayWorks(works);
+});
+
+fetchCategories().then((categories) => {
+  createFilterButtons(categories);
+});
